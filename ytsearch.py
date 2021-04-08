aca4a49dfe93e050cfce95baaeea968066e59580
Ryan Hubler <ryanhubler@ryanhubler.com>
2021-04-08 00:30:52 -0400

    Create ytsearch.py

---
+++ ytsearch.py
@@ -0,0 +1,7 @@
+from youtubesearchpython import * #used to take user input and search youtube and returns url to be used by youtube_dl
+def search(term):
+    videosSearch = VideosSearch(term, limit = 1, language = 'en', region = 'US') #takes term(user input from discord) and searched youtube and returns a messed up dictonary 
+    results= videosSearch.result(mode = ResultMode.dict) #makes the output of the search a dict 
+    unfinishedlink = str(results['result']).strip('[]') #remove the messed up parts of the returned dict form the search results
+    link = eval(unfinishedlink) #makes the varible unfinisedlink back into a dict (could technicly be unsafe)
+    return [link['link']]
