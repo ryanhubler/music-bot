2b46b027f83ed19178dcf273266103ceb5ca30bd
Ryan Hubler <ryanhubler@ryanhubler.com>
2021-06-17 02:10:56 -0400

    Added queue feature

--- bot.py
+++ bot.py
@@ -2,30 +2,39 @@ import discord
 from discord.ext import commands
 from ytdownloader import *
 from ytsearch import  *
+from songqueue import *
+import asyncio
 token = ''   #your token goes here
 bot = commands.Bot(command_prefix='/') #Change prefix used to call bot in discord 
 
 @bot.command()
-async def play(ctx, *, mess): #play is the argument used after the user types the prefix
-    isconnected = str(ctx.voice_client)
+async def play(ctx,*,mess): #play is the argument used after the user types the prefix
     if ctx.author.voice != None:
-        if isconnected == "None":
-            channel = ctx.author.voice.channel #makes the channel varible = to the channel name that the person who used the command is in 
-            yt_download(search(mess))
-            vc = await channel.connect() #define vc for use in the next line
+        music_queue(mess)
+        channel = ctx.author.voice.channel #makes the channel varible = to the channel name that the person who used the command is in 
+        vc = await channel.connect() #define vc for use in the next line
+        while len(songs) != 0:
+            new_song = songs.pop(0)
+            yt_download(search(new_song))
             vc.play(discord.FFmpegPCMAudio("song.mp3"),after=lambda e: print('done', e)) #uses ffmpeg to play "song" aka test.mp3 
-            await ctx.send("Now Playing"+ str(search(mess)))
-        else:
-            await leave(ctx)
-            channel = ctx.author.voice.channel #makes the channel varible = to the channel name that the person who used the command is in 
-            yt_download(search(mess))
-            vc = await channel.connect() #define vc for use in the next line
-            vc.play(discord.FFmpegPCMAudio("song.mp3"),after=lambda e: print('done', e)) #uses ffmpeg to play "song" aka test.mp3 
-            await ctx.send("Now Playing" + str(search(mess)))
+            await ctx.send("Now Playing"+ str(search(new_song)))
+            await asyncio.sleep(int(time_search(new_song)) + 3)
+        if len(songs) == 0:
+            await leave(ctx)   
     else:
-        await ctx.send("You are not in a voice channel")
+        await ctx.send("You are not in a voice channel or a voice channel I cannot access")
+@bot.command()
+async def queue(ctx, *, mess):
+    music_queue(mess)
+    print(len(songs))
+
+@bot.command()
+async def join(ctx):
+    channel = ctx.author.voice.channel #makes the channel varible = to the channel name that the person who used the command is in 
+    vc = await channel.connect() #define vc for use in the next line
+    
 @bot.command()
 async def leave(ctx):
-    await ctx.voice_client.disconnect()  
-
+    await ctx.voice_client.disconnect() 
+    
 bot.run(token)
--- ytsearch.py
+++ ytsearch.py
@@ -5,3 +5,13 @@ def search(term):
     unfinishedlink = str(results['result']).strip('[]') #remove the messed up parts of the returned dict form the search results
     link = eval(unfinishedlink) #makes the varible unfinisedlink back into a dict (could technicly be unsafe)
     return [link['link']]
+
+def time_search(term):
+    videosSearch = VideosSearch(term, limit = 1, language = 'en', region = 'US') #takes term(user input from discord) and searched youtube and returns a messed up dictonary 
+    results= videosSearch.result(mode = ResultMode.dict) #makes the output of the search a dict 
+    unfinishedlink = str(results['result']).strip('[]') #remove the messed up parts of the returned dict form the search results
+    link = eval(unfinishedlink) #makes the varible unfinisedlink back into a dict (could technicly be unsafe)
+    minsec = str([link['duration']]).strip("[ '' ]")
+    m, s = minsec.split(':')
+    duration = int(m) * 60 + int(s)
+    return duration
