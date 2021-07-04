92037e7481715d9950d4d17bd5d87a34fa3e2c2c
Ryan Hubler <ryanhubler@ryanhubler.com>
2021-07-04 01:40:40 -0400

    Update ytsearch.py

--- ytsearch.py
+++ ytsearch.py
@@ -15,3 +15,9 @@ def time_search(term):
     m, s = minsec.split(':')
     duration = int(m) * 60 + int(s)
     return duration
+def name_search(term):
+    videosSearch = VideosSearch(term, limit = 1, language = 'en', region = 'US') #takes term(user input from discord) and searched youtube and returns a messed up dictonary 
+    results= videosSearch.result(mode = ResultMode.dict) #makes the output of the search a dict 
+    unfinishedlink = str(results['result']).strip('[]') #remove the messed up parts of the returned dict form the search results
+    link = eval(unfinishedlink) #makes the varible unfinisedlink back into a dict (could technicly be unsafe)
+    return [link['title']]
